aea26a061912e16e69643eca00bbf2996e875a5a
guilherme C Santos <guicarsiqsantos@gmail.com>
2019-11-29 13:57:17 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,11 @@
+/**
+ * Author: Alessandro luiz Santos
+ * https://github.com/aleluizsantos
+ */
+
+import React from 'react';
+import Routes from './src/router';
+
+export default function App() {
+  return <Routes />
+}
